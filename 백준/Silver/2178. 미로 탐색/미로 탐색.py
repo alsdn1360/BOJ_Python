87043aa09c279a8cda91863efaffed52d4fdfa06
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2025-02-19 23:19:49 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,12 +1,10 @@
 from collections import deque
 
+MOVES = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-def bfs(end):
-    queue = deque()
-    queue.append((0, 0, 1))
+def bfs(end, maze):
+    queue = deque([(0, 0, 1)])
     maze[0][0] = 0
-
-    moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
     while queue:
         x, y, steps = queue.popleft()
@@ -14,9 +12,8 @@ def bfs(end):
         if x == end[0] and y == end[1]:
             return steps
 
-        for dx, dy in moves:
-            nx = x + dx
-            ny = y + dy
+        for dx, dy in MOVES:
+            nx, ny = x + dx, y + dy
 
             if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] == 1:
                 queue.append((nx, ny, steps + 1))
@@ -27,4 +24,4 @@ def bfs(end):
 n, m = map(int, input().split())
 maze = [list(map(int, input().strip())) for _ in range(n)]
 
-print(bfs((n - 1, m - 1)))
+print(bfs((n - 1, m - 1), maze))
