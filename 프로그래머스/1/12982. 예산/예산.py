f97bb0c1cc718c7281a5048414651fb39717b1da
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2025-03-21 13:29:08 +0900

    [level 1] Title: 예산, Time: 1.16 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,26 @@
+def solution(d, budget):
+    answer = 0
+    
+    d.sort()
+    n = len(d)
+    
+    for i in range(n):
+        cnt = 0
+            
+        # 첫 번째 값이 budget보다 작거나 같아야 빼고 cnt +1
+        if budget >= d[i]:
+            temp_budget = budget - d[i]
+            cnt += 1
+        else:
+            temp_budget = budget
+        
+        # 그 다음 값부터 계속 빼면서 0보다 크면 cnt +1
+        for j in range(i + 1, n):
+            if temp_budget - d[j] >= 0:
+                temp_budget -= d[j]
+                cnt += 1
+                
+        # 이전의 최대값이랑 지금의 카운트 중에 큰 값으로 갱신
+        answer = max(answer, cnt)
+                
+    return answer
