e05c505bf706664f2024241954f591b568dcfcd1
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2025-03-28 11:13:15 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -3,12 +3,12 @@ def solution(word):
     
     alpha = ['A', 'E', 'I', 'O', 'U']
     
-    # 해당 인덱스에 알파벳을 정하면 그 뒤에 올 알파벳의 개수
+    # 그 자리 뒤에 올 수 있는 모든 가능한 단어들의 개수
     # [(5⁰ + 5¹ + 5² + 5³ + 5⁴), (5⁰ + 5¹ + 5² + 5³), ...]
-    # 각 알파벳마다 781번 건너뛰게 됨
     weight = [781, 156, 31, 6, 1]
     
     for i, w in enumerate(word):
+		    # 각 알파벳마다 781번 건너뛰게 되므로 알파벳의 인덱스를 곱해줌
         answer += alpha.index(w) * weight[i]
     
     return answer + len(word)
