f4a21a9ca89ae2c1d09295f7a8d6b7ecad042978
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2025-04-16 22:03:19 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 964.03 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,29 @@
+import math
+
+def solution(r1, r2):
+    answer = 0
+    
+    for x in range(0, r2 + 1):
+        # r1^2 - x^2 <= y^2 <= r2^2 - x^2 공식을 이용해서 갯수를 셈
+        min_y = math.ceil(math.sqrt(max(0, r1*r1 - x*x)))
+        max_y = math.floor(math.sqrt(r2*r2 - x*x))
+        
+        if min_y > max_y:
+            continue
+        
+        # 둘 다 0이 아니면 4배, x나 y 중 0이 있으면 2배
+        if x == 0:
+            answer += 2 * (max_y - min_y + 1)
+        else:
+            if min_y == 0:
+                # (x, 0), (-x, 0)
+                answer += 2
+                
+                if max_y >= 1:
+                    # 0 빼고 max_y까지 갯수
+                    answer += 4 * max_y
+            else:
+                # y가 전부 양수일 때
+                answer += 4 * (max_y - min_y + 1)
+
+    return answer
