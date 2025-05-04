60cb62c8dd8871b4db8d78a8d1b2e79ed1ccdfb6
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-05-04 15:10:07 +0900

    [level 2] Title: 디펜스 게임, Time: 435.11 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 52.4 MB, 시간: 798.35 ms
+메모리: 52.4 MB, 시간: 435.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 04일 14:57:00
+2025년 05월 04일 15:10:05
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -4,26 +4,24 @@ def solution(n, k, enemy):
     answer = 0
     heap = []
     
-    # 두 가지 경우가 있음 => 1. 병사 사용 2. 무적권 사용
     for e in enemy:
-        if n < e and k == 0:
-            return answer
+        # 병사 사용
+        n -= e
         
-        # 맥스 힙 사용
-        heapq.heappush(heap, (-e, e))
-            
-        if n >= e:
-            # 병사 사용
-            n -= e
-            answer += 1
-        else:
-            if heap and k != 0:
-                # 무적권 사용 -> 이전 라운드에서 가장 많이 사용한 병사 되돌리기
+        # 맥스힙 푸시
+        heapq.heappush(heap, -e)
+        
+        if n < 0:
+            if k > 0:
+                # 무적권 사용
+                n += -heapq.heappop(heap)
                 k -= 1
-                n += heapq.heappop(heap)[1]
-                
-                # 되돌린 병사로 현재 라운드 막기
-                n -= e
-                answer += 1
-                
+            else:
+                # 모든 라운드를 막지 못했을 때, 종료
+                return answer
+            
+        answer += 1
+        
+    # 모든 라운드를 막고 종료
     return answer
+        
