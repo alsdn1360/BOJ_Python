8c66cbd457bb9d2dbfe63b4f325a0e006e99347c
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-05-07 16:34:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 307.42 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
@@ -1,18 +1,20 @@
 class Solution {
     fun solution(elements: IntArray): Int {
         val answer = mutableSetOf<Int>()
-        val n = elements.size
         
-        for (len in 1..n) {
-            for (i in 0 until n) {
-                var sum = 0
-                
-                for (j in i until (i + len)) {
-                    sum += elements[j % n]
-                }
-                
-                answer.add(sum)
-            }   
+        val n = elements.size
+        val extended = elements + elements
+        
+        val prefix = IntArray(2 * n + 1)
+        
+        for (i in 0 until 2 * n) {
+            prefix[i + 1] = prefix[i] + extended[i]
+        }
+        
+        for (i in 0 until n) {
+            for (len in 1..n) {
+                answer.add(prefix[i + len] - prefix[i])
+            }
         }
         
         return answer.size
