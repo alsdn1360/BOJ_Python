457fbd144f82010534f499759beb50fd118fdc17
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-05-11 11:30:44 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,25 +1,11 @@
 def solution(skill, skill_trees):
     answer = 0
     
-    # 스킬트리를 리스트로 변환
-    skill_trees = [list(st) for st in skill_trees]
-    
     for skill_tree in skill_trees:
-        idx = 0 # 스킬의 순서 인덱스
-        able = True # 스킬트리 가능 여부
+        # 현재 스킬트리에서 스킬에 속하는 부분만 추출
+        filtered_skill_tree = ''.join([s for s in skill_tree if s in skill])
         
-        for s in skill_tree:
-            if s not in skill:
-                continue
-                
-            # 배워야할 스킬의 순서가 아닐 때
-            if s != skill[idx]:
-                able = False
-                break
-                
-            idx += 1
-            
-        if able:
+        if filtered_skill_tree == skill[:len(filtered_skill_tree)]:
             answer += 1
             
     return answer
