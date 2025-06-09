02b4cb474fd7a78eef0835bcf7ae2a9d074dbb1e
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-06-09 14:38:48 +0900

    [Silver I] Title: 문자열 교환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.py
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.py
@@ -1,16 +1,16 @@
 # main
-ab = list(input())
+ab = input()
 
 n = len(ab)
 window_size = ab.count("a")
 
-ab += ab  # 원형으로 되어있기 때문에 뒤에 한번 더해줌
+extended_ab = ab + ab  # 원형으로 되어있기 때문에 뒤에 한번 더해줌
 
 answer = float("inf")
 
 for i in range(n):
     # 윈도우 안에서 b의 개수만 구해주면 a는 모두 연속으로 이어짐
     # 이때의 b의 개수의 최소값을 구하면 됨
-    answer = min(answer, ab[i : i + window_size].count("b"))
+    answer = min(answer, extended_ab[i : i + window_size].count("b"))
 
 print(answer)
