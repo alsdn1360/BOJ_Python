4dadfa3d2fd1041c68c4961e6275da0b73718c6e
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-06-19 15:07:24 +0900

    [Gold V] Title: 회의실 배정, Time: 2708 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,24 +1,21 @@
 # main
 n = int(input())
 
-answer = 0
-times = []
+meetings = []
 
 for _ in range(n):
     start, end = map(int, input().split())
-
-    times.append((start, end))
+    meetings.append((start, end))
 
 # 회의가 가장 빨리 끝나는 순으로 정렬
-times.sort(key=lambda x: (x[1], x[0]))
+meetings.sort(key=lambda x: (x[1], x[0]))
 
+answer = 0
 prev_end = 0
 
-for time in times:
-    curr_start = time[0]
-
-    if curr_start >= prev_end:
+for start, end in meetings:
+    if start >= prev_end:
         answer += 1
-        prev_end = time[1]
+        prev_end = end
 
 print(answer)
