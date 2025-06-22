33943bffca2dac5e61b8192c3457e7b6e32efc7c
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-06-22 16:50:55 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,27 +1,21 @@
 # main
 def bt(start):
     if len(answer) == m:
-        print(*sorted(answer))
-        prev.add(tuple(answer))
-
+        print(*answer)
         return
 
-    for i in range(start, n):
-        num = nums[i]
+    for i in range(start, len(nums)):
+        answer.append(nums[i])
 
-        answer.append(num)
-
-        if tuple(answer) not in prev:
-            bt(i)
+        bt(i)
 
         answer.pop()
 
 
 # main
 n, m = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
+nums = sorted(list(set(map(int, input().split()))))
 
 answer = []
-prev = set()
 
 bt(0)
