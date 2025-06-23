555350c3c5b3a4e244a72798083d78a7ac38576e
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-06-23 15:47:03 +0900

    [Silver I] Title: RGB거리, Time: 68 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,18 @@
+# main
+n = int(input())
+
+# house[i][0]: 빨강 칠하는 비용
+# house[i][1]: 초록 칠하는 비용
+# house[i][2]: 파랑 칠하는 비용
+house = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * 3 for _ in range(n)]
+
+dp[0][0], dp[0][1], dp[0][2] = house[0][0], house[0][1], house[0][2]
+
+for i in range(1, n):
+    dp[i][0] = house[i][0] + min(dp[i - 1][1], dp[i - 1][2])
+    dp[i][1] = house[i][1] + min(dp[i - 1][0], dp[i - 1][2])
+    dp[i][2] = house[i][2] + min(dp[i - 1][0], dp[i - 1][1])
+
+print(min(dp[n - 1]))
