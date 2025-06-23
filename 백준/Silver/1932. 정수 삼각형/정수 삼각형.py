288e36dc7618c870aee0756257b0bf9524d5816b
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-06-23 14:45:19 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,17 +1,8 @@
-# main
 n = int(input())
-
 nums = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(1, n):
-    nums_j = len(nums[i])
+for i in range(n - 2, -1, -1):
+    for j in range(i + 1):
+        nums[i][j] += max(nums[i + 1][j], nums[i + 1][j + 1])
 
-    for j in range(nums_j):
-        if j == 0:
-            nums[i][j] += nums[i - 1][j]
-        elif j == nums_j - 1:
-            nums[i][j] += nums[i - 1][j - 1]
-        else:
-            nums[i][j] += max(nums[i - 1][j], nums[i - 1][j - 1])
-
-print(max(nums[n - 1]))
+print(nums[0][0])
