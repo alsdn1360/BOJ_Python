0d4c98c9fb5e03beff1d94f2dba329393122b2ba
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-07-08 13:19:28 +0900

    [Gold II] Title: 트리의 지름, Time: 856 ms, Memory: 93360 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -4,23 +4,24 @@ input = sys.stdin.readline
 
 
 def dijkstra(start):
-    dists = {node: float("inf") for node in range(1, v + 1)}
-    dists[start] = 0
+    dists = [float("inf")] * (v + 1)
+    dists[0], dists[start] = -1, 0
 
     queue = []
-    heapq.heappush(queue, (start, 0))
+    heapq.heappush(queue, (0, start))
 
     while queue:
-        curr_node, curr_dist = heapq.heappop(queue)
+        curr_dist, curr_node = heapq.heappop(queue)
 
         if dists[curr_node] < curr_dist:
             continue
 
         for next_node, next_dist in tree[curr_node]:
             dist = curr_dist + next_dist
+
             if dists[next_node] > dist:
                 dists[next_node] = dist
-                heapq.heappush(queue, (next_node, dist))
+                heapq.heappush(queue, (dist, next_node))
 
     return dists
 
@@ -32,26 +33,15 @@ tree = {node: [] for node in range(1, v + 1)}
 for _ in range(v):
     edges = list(map(int, input().split()))
 
-    ends = []
-    dists = []
+    curr_node = edges[0]
 
-    for i in range(1, len(edges) - 1):
-        if i % 2 != 0:
-            ends.append(edges[i])
-        else:
-            dists.append(edges[i])
+    for i in range(1, len(edges) - 2, 2):
+        adj_node = edges[i]
+        dist = edges[i + 1]
+        tree[curr_node].append((adj_node, dist))
 
-    for i in range(len(ends)):
-        tree[edges[0]].append((ends[i], dists[i]))
-        tree[ends[i]].append((edges[0], dists[i]))
+dists_from_random = dijkstra(1)
+fartest_node = dists_from_random.index(max(dists_from_random))
+dists_froom_fartest_node = dijkstra(fartest_node)
 
-# 루트로 부터 모든 노드까지의 거리
-costs_from_root = dijkstra(1)
-
-# 그 중에서 가장 먼 노드
-fartest_node = max(costs_from_root, key=costs_from_root.get)
-
-# 가장 먼 노드에서 모든 노드까지의 거리
-costs_from_fartest_node = dijkstra(fartest_node)
-
-print(max(costs_from_fartest_node.values()))
+print(max(dists_froom_fartest_node))
