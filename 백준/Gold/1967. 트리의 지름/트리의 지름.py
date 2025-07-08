a626a5be2e829dfbb8bb531d54a4d57891d3c2e0
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-07-08 13:26:15 +0900

    [Gold IV] Title: 트리의 지름, Time: 72 ms, Memory: 38580 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -3,46 +3,41 @@ import sys, heapq
 input = sys.stdin.readline
 
 
-def dijkstra(tree, start):
-    costs = {i: float("inf") for i in range(1, n + 1)}
-    costs[start] = 0
+def dijkstra(start):
+    dists = [float("inf")] * (n + 1)
+    dists[0], dists[start] = -1, 0
 
     queue = []
-    heapq.heappush(queue, (start, 0))
+    heapq.heappush(queue, (0, start))
 
     while queue:
-        curr_node, curr_cost = heapq.heappop(queue)
+        curr_dist, curr_node = heapq.heappop(queue)
 
-        if costs[curr_node] < curr_cost:
+        if dists[curr_node] < curr_dist:
             continue
 
-        for dest_node, new_cost in tree[curr_node]:
-            cost = curr_cost + new_cost
+        for next_node, next_dist in tree[curr_node]:
+            dist = curr_dist + next_dist
 
-            if costs[dest_node] > cost:
-                costs[dest_node] = cost
-                heapq.heappush(queue, (dest_node, cost))
+            if dists[next_node] > dist:
+                dists[next_node] = dist
+                heapq.heappush(queue, (dist, next_node))
 
-    return costs
+    return dists
 
 
 # main
 n = int(input())
 
-tree = {i: [] for i in range(1, n + 1)}
+tree = {node: [] for node in range(1, n + 1)}
 
 for _ in range(n - 1):
     u, v, w = map(int, input().split())
     tree[u].append((v, w))
     tree[v].append((u, w))
 
-# 루트로 부터 모든 노드까지의 거리
-costs_from_root = dijkstra(tree, 1)
+dists = dijkstra(1)
+fartest_node = dists.index(max(dists))
+dists_froom_fartest_node = dijkstra(fartest_node)
 
-# 그 중에서 가장 먼 노드
-fartest_node = max(costs_from_root, key=costs_from_root.get)
-
-# 가장 먼 노드에서 모든 노드까지의 거리
-costs_from_fartest_node = dijkstra(tree, fartest_node)
-
-print(max(costs_from_fartest_node.values()))
+print(max(dists_froom_fartest_node))
