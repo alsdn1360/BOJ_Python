20b7e2e7cbdc824b85355da8d7758d1e44dbd87c
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-07-08 14:56:47 +0900

    [Gold II] Title: 후위 표기식, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -1,53 +1,38 @@
+def operator(op):
+    if op == "(":
+        return 0
+    elif op == "+" or op == "-":
+        return 1
+    else:
+        return 2
+
+
 # main
 notation = input()
 
-postfix = []
+postfix = ""
 stack = []
 
 for c in notation:
     # 문자열은 바로 postfix에 넣음
-    if 65 <= ord(c) <= 90:
-        postfix.append(c)
+    if "A" <= c <= "Z":
+        postfix += c
+    # 여는 괄호는 무조건 스택에 넣음
+    elif c == "(":
+        stack.append(c)
+    # 닫는 괄호면 여는 괄호가 나올 때까지 연산자를 postfix에 넣음
+    elif c == ")":
+        while stack[-1] != "(":
+            postfix += stack.pop()
+
+        stack.pop()  # ( 버리기
     else:
-        # 여는 괄호는 무조건 스택에 넣음
-        if c == "(":
-            stack.append(c)
-        # 닫는 괄호면 여는 괄호가 나올 때까지 연산자를 postfix에 넣음
-        elif c == ")":
-            while stack:
-                if stack[-1] == "(":
-                    stack.pop()
-                    break
+        while stack and operator(stack[-1]) >= operator(c):
+            postfix += stack.pop()
 
-                postfix.append(stack.pop())
-        else:
-            if not stack:
-                stack.append(c)
-            else:
-                # 스택 top보다 우선순위가 높은 연산자면 스택에 넣음
-                if c == "*" or c == "/":
-                    if stack[-1] == "(" or stack[-1] == "+" or stack[-1] == "-":
-                        stack.append(c)
-                    else:
-                        # 낮은 우선순위가 나올 때까지 계속 postfix에 넣음
-                        while stack:
-                            if stack[-1] == "(" or stack[-1] == "+" or stack[-1] == "-":
-                                break
-
-                            postfix.append(stack.pop())
-
-                        stack.append(c)
-                else:
-                    # '+', '-' 보다 낮은 우선순위는 괄호밖에 없음
-                    while stack:
-                        if stack[-1] == "(":
-                            break
-
-                        postfix.append(stack.pop())
-
-                    stack.append(c)
+        stack.append(c)
 
 while stack:
-    postfix.append(stack.pop())
+    postfix += stack.pop()
 
 print("".join(postfix))
