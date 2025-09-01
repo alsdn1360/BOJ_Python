dc2edc99e6e035f331e781bf17ce3633f6d2abe6
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-09-01 14:28:46 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,11 @@
+# main
+n = int(input())
+nums = list(map(int, input().split()))
+
+dp = [-1001] * n
+dp[0] = nums[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + nums[i], nums[i])
+
+print(max(dp))
