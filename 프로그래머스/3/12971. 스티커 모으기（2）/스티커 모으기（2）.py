ec8cc5ea1eae59b5604643a01bb0d2ec686141f1
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-09-05 13:17:04 +0900

    [level 3] Title: 스티커 모으기(2), Time: 68.29 ms, Memory: 13.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,23 @@
+def solve(sticker):
+    n = len(sticker)
+    
+    dp = [0] * n
+    
+    dp[0] = sticker[0]
+    dp[1] = max(dp[0], sticker[1])
+    
+    for i in range(2, n):
+        dp[i] = max(dp[i - 1], dp[i - 2] + sticker[i]) # 현재 스티커 선택 X vs 선택 O
+        
+    return max(dp)
+    
+
+def solution(sticker):
+    if len(sticker) == 1 or len(sticker) == 2:
+        return max(sticker)
+        
+    
+    case1 = solve(sticker[:-1]) # 첫 번째 스티커를 뜯었을 때, 마지막 스티커는 선택 못하므로 아예 없앰
+    case2 = solve(sticker[1:]) # 마지막 스티커를 뜯었을 때, 첫 번재 스티커는 선택 못하므로 아예 없앰
+    
+    return max(case1, case2)
