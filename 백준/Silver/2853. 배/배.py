f4f1765a119a4c38d7497aed248bf963cc7a23cf
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-09-10 14:30:37 +0900

    [Silver I] Title: 배, Time: 1068 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2853. 배/배.py
+++ 백준/Silver/2853. 배/배.py
@@ -5,14 +5,15 @@ input = sys.stdin.readline
 # main
 n = int(input())
 happy_day = [int(input()) for _ in range(n)]
+
+for i in range(n):
+    happy_day[i] -= 1
+
 happy_day.pop(0)
 
 periods = set()
 
-ans = 0
-
-for day in happy_day:
-    arrival_day = day - 1
+for arrival_day in happy_day:
     is_new_period = True
 
     # 이미 있는 주기로 나누어 떨어지면 새로운 주기의 배가 아님
@@ -24,6 +25,5 @@ for day in happy_day:
     # 새로운 주기의 배의 주기만 넣음
     if is_new_period:
         periods.add(arrival_day)
-        ans += 1
 
-print(ans)
+print(len(periods))
