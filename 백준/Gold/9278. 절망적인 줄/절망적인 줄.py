4433dc9ad52a58602886eb3138a4bcca606bfc24
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-09-11 14:40:58 +0900

    [Gold IV] Title: 절망적인 줄, Time: 1864 ms, Memory: 240768 KB -BaekjoonHub

---
+++ 백준/Gold/9278. 절망적인 줄/절망적인 줄.py
@@ -0,0 +1,44 @@
+import sys
+
+input = sys.stdin.readline
+
+# main
+while True:
+    try:
+        line = input().rstrip()
+
+        if not line:
+            break
+
+        # 가장 첫 번째 사람이 100원이면 방법이 없음
+        if line[0] == ")":
+            print(0)
+            continue
+
+        n = len(line)
+
+        # i번째 사람까지 줄을 세웠을 때, 그 중 50원을 들고있는 사람이 j명인 경우의 수
+        dp = [[0 for _ in range(n // 2 + 1)] for _ in range(n + 1)]
+
+        # 아무도 없는 경우의 수를 1으로 지정
+        dp[0][0] = 1
+
+        for i in range(1, n + 1):
+            char = line[i - 1]
+
+            for j in range(n // 2 + 1):
+                # 50원인 사람의 경우
+                if (char == "." or char == "(") and j > 0:
+                    dp[i][j] += dp[i - 1][j - 1]
+
+                # 100원인 사람의 경우
+                # 50원을 가지고 있는 사람이 100원을 가지고 있는 사람보다 많거나 같아야 함
+                if (char == "." or char == ")") and j >= i - j:
+                    dp[i][j] += dp[i - 1][j]
+
+        ans = dp[n][n // 2]
+
+        print(ans % 1000000)
+
+    except EOFError:
+        break
