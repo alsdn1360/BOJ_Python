0d99e888c44e3d8ed727e4148135a57fddf23c33
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-09-18 14:33:39 +0900

    [Silver IV] Title: 사이클 단어, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1544. 사이클 단어/사이클 단어.py
+++ 백준/Silver/1544. 사이클 단어/사이클 단어.py
@@ -12,11 +12,10 @@ cycle_words = []
 for word in words:
     is_new = True
 
-    if cycle_words:
-        for cycle_word in cycle_words:
-            if word in (cycle_word * 2) and len(word) == len(cycle_word):
-                is_new = False
-                break
+    for cycle_word in cycle_words:
+        if word in (cycle_word * 2) and len(word) == len(cycle_word):
+            is_new = False
+            break
 
     if is_new:
         cycle_words.append(word)
