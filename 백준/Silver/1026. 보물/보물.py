f2989f7dcc1b938aa4d16af2735b8fbe96c5ce5b
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-10-01 14:12:37 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+# main
+n = int(input())
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+ans = 0
+
+for i in range(n):
+    ans += a[i] * b[i]
+
+print(ans)
