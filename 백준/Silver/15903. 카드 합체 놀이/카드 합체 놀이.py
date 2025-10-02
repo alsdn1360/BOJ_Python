a8c43859c4b988ece141bbc3dc49e43cc4a0b52c
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-10-02 14:06:03 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 52 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,10 +1,20 @@
+import sys, heapq
+
+input = sys.stdin.readline
+
 # main
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
+heapq.heapify(a)
+
 for _ in range(m):
-    a.sort()
-    temp = a[0] + a[1]
-    a[0], a[1] = temp, temp
+    x = heapq.heappop(a)
+    y = heapq.heappop(a)
+
+    temp = x + y
+
+    heapq.heappush(a, temp)
+    heapq.heappush(a, temp)
 
 print(sum(a))
