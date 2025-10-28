0e00cac0c86d3dda32d9d898a141c22b6f983bd4
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-10-28 15:02:12 +0900

    [Gold V] Title: 트리 색칠하기, Time: 724 ms, Memory: 100440 KB -BaekjoonHub

--- 백준/Gold/24230. 트리 색칠하기/트리 색칠하기.py
+++ 백준/Gold/24230. 트리 색칠하기/트리 색칠하기.py
@@ -1,26 +1,36 @@
-from collections import defaultdict
+from collections import deque, defaultdict
 import sys
-
-sys.setrecursionlimit(10**9)
 
 input = sys.stdin.readline
 
 
-def dfs(parent_node, parent_color):
-    global answer
+def bfs():
+    # 루트의 색이 하얀색이 아니면 일단 한 번 칠해줘야 함
+    if colors[0] != 0:
+        answer = 1
+    else:
+        answer = 0
 
-    for adj_node in graph[parent_node]:
-        if adj_node in visited:
-            continue
+    queue = deque([(1, colors[0])])
+    visited = set()
+    visited.add(1)
 
-        visited.add(adj_node)
+    while queue:
+        parent_node, parent_color = queue.popleft()
 
-        target_color = colors[adj_node - 1]
+        for adj_node in graph[parent_node]:
+            if adj_node in visited:
+                continue
 
-        if parent_color != target_color:
-            answer += 1
+            target_color = colors[adj_node - 1]
 
-        dfs(adj_node, colors[adj_node - 1])
+            if parent_color != target_color:
+                answer += 1
+
+            queue.append((adj_node, target_color))
+            visited.add(adj_node)
+
+    return answer
 
 
 # main
@@ -35,15 +45,4 @@ for _ in range(n - 1):
     graph[u].append(v)
     graph[v].append(u)
 
-# 루트의 색이 하얀색이 아니면 일단 한 번 칠해줘야 함
-if colors[0] != 0:
-    answer = 1
-else:
-    answer = 0
-
-visited = set()
-visited.add(1)
-
-dfs(1, colors[0])
-
-print(answer)
+print(bfs())
