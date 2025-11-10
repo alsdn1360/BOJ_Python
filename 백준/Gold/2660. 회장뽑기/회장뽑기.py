5baa2e1152b8a1038686f6a289c48a3172452c6a
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-11-10 13:50:02 +0900

    [Gold V] Title: 회장뽑기, Time: 56 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.py
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.py
@@ -5,21 +5,20 @@ input = sys.stdin.readline
 
 
 def bfs(start):
-    queue = deque([(start, 1)])  # 시작 회원, 친구 몇 단계인지
-    visited = set()
-    visited.add(start)
+    queue = deque([(start, 0)])  # 시작 회원, 친구 몇 단계인지
+    visited = {start}
 
     max_step = 0
 
     while queue:
         curr_member, step = queue.popleft()
 
+        max_step = max(max_step, step)
+
         for friend in members[curr_member]:
             if friend not in visited:
                 queue.append((friend, step + 1))
                 visited.add(friend)
-
-                max_step = max(max_step, step)
 
     # 현재 회원에게서 각 회원에게로 갈 수 있는 거리(친구 단계) 중에서 가장 큰 거리가 점수임
     return max_step
