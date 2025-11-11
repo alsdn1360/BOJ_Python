8c635da9d65db36ab5bd06eac44854d1b5cb07c5
Kang Dongwoo <110466553+alsdn1360@users.noreply.github.com>
2025-11-11 14:58:56 +0900

    [Silver III] Title: 자리배정, Time: 616 ms, Memory: 68272 KB -BaekjoonHub

---
+++ 백준/Silver/10157. 자리배정/자리배정.py
@@ -0,0 +1,33 @@
+import sys
+
+# 하, 우, 상, 좌
+MOVES = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+def in_bound(nx, ny):
+    return 0 <= nx < r and 0 <= ny < c
+
+
+# main
+c, r = map(int, input().split())
+k = int(input())
+
+if c * r < k:
+    print(0)
+    sys.exit()
+
+seats = [[0 for _ in range(c)] for _ in range(r)]
+
+x, y, dir = 0, 0, 0
+dx, dy = MOVES[dir]
+
+for cnt in range(1, k):
+    seats[x][y] = cnt
+
+    if not in_bound(x + dx, y + dy) or seats[x + dx][y + dy] != 0:
+        dir = (dir + 1) % 4
+        dx, dy = MOVES[dir]
+
+    x, y = x + dx, y + dy
+
+print(y + 1, x + 1)
