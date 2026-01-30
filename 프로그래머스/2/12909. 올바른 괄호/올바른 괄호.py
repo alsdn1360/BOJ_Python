3cb16eee31e73ab3e1e085bf7b53a2612943eab6
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-01-30 21:11:36 +0900

    [level 2] Title: 올바른 괄호, Time: 9.67 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,13 @@
 def solution(s):
     stack = []
     
-    for c in s:
-        if c == '(':
-            stack.append(c)
-        elif c == ')':
-            if stack and stack[-1] == '(':
-                stack.pop()
-            else:
-                return False
-                
-    if not stack: # 스택이 비어있다면
-        return True
-    else:
-        return False
+    for p in s:   
+        if stack and stack[-1] == '(' and p == ')' :
+            stack.pop()
+            
+            continue
+            
+        stack.append(p)
+                    
+    return True if not stack else False
+        
