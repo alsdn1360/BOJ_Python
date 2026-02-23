a9dc1470422af953827ff570b6594690accf5e82
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-23 17:25:51 +0900

    [level 2] Title: 짝지어 제거하기, Time: 115.24 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,13 +1,10 @@
 def solution(s):
     stack = []
     
-    for c in s:
-        if stack and stack[-1] == c:
+    for alp in s:
+        if stack and stack[-1] == alp:
             stack.pop()
         else:
-            stack.append(c)
+            stack.append(alp)
             
-    if not stack:
-        return 1
-    else:
-        return 0
+    return 1 if not stack else 0
