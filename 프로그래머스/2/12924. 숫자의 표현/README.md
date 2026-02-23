6398875a1b9c7a3db214162b067f4ee3908d961d
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-23 16:01:09 +0900

    [level 2] Title: 숫자의 표현, Time: 3.18 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 1.92 ms
+메모리: 9.32 MB, 시간: 3.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 14일 13:13:16
+2026년 02월 23일 16:01:06
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,24 +1,21 @@
 def solution(n):
     answer = 0
     
-    # 투 포인터 사용
-    i, j = 1, 1
-    total = 0
+    nums = [i for i in range(1, n + 1)]
+    l, r = 0, 0
     
-    while i <= n:
-        # 현재 합이 n보다 작으면 j 이동
-        if total < n:
-            total += j
-            j += 1
-        # 현재 합이 n이면 방법수 +1 하고, i 이동
-        elif total == n:
-            answer += 1
-            
-            total -= i
-            i += 1
-        # 현재 합이 n보다 작아질 때까지 i 이동
+    curr_sum = sum(nums[l:r])
+    
+    while l < n and r <= n:            
+        if curr_sum < n:
+            curr_sum += nums[r]
+            r += 1
+        elif curr_sum > n:
+            curr_sum -= nums[l]
+            l += 1
         else:
-            total -= i
-            i += 1
+            answer += 1
+            curr_sum -= nums[l]
+            l += 1
             
     return answer
