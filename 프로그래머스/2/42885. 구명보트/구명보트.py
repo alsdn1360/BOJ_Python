7f2a2608d520e075c106b48cd34d35fcc77b794e
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-23 22:01:47 +0900

    [level 2] Title: 구명보트, Time: 9.57 ms, Memory: 9.72 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,28 +1,20 @@
 from collections import deque
 
 def solution(people, limit):
-    answer = 0
+    n = len(people)
+    answer = n
     
-    # 무게 최소값과 최대값을 더하기 위해 정렬
-    people.sort()
-    queue = deque()
+    people.sort(reverse=True)
+    people = deque(people)
     
-    for p in people:
-        queue.append(p)
-    
-    while len(queue) > 1:
-        # 무게 최대값과 최소값을 더해서 limit을 넘으면 무게 최대값을 가진 사람만 태워서 구출
-        if queue[0] + queue[-1] > limit:
-            queue.pop()
-            answer += 1
-        # 둘 다 합쳐서 limit을 안넘으면 둘 다 구출
-        else:
-            queue.pop()
-            queue.popleft()
-            answer += 1
-            
-    # 만약 한 명만 남아있으면 그 한 명만 구출
-    if len(queue) == 1:
-        answer += 1
+    while people:
+        w = people.popleft()
         
+        if people:
+            remain_w = limit - w
+
+            if remain_w >= people[-1]:
+                people.pop()
+                answer -= 1
+
     return answer
