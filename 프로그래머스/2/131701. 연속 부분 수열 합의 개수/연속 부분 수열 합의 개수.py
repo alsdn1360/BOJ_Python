582df41f25051a178cfeeaa39e4c8a2bb77d8d89
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-24 17:45:03 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 299.23 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -2,17 +2,15 @@ def solution(elements):
     answer = set()
     
     n = len(elements)
-    extended = elements * 2 # 원형 수열로 변경
+    elements *= 2
     
-    # 누적합 이용
     prefix = [0] * (2 * n + 1)
     
     for i in range(2 * n):
-        prefix[i + 1] = prefix[i] + extended[i]
-        
+        prefix[i + 1] = prefix[i] + elements[i]
+    
     for i in range(n):
-        # 길이별로 누적합에서 값을 가져옴
-        for length in range(1, n + 1):
-            answer.add(prefix[i + length] - prefix[i])
+        for l in range(1, n + 1):
+            answer.add(prefix[i + l] - prefix[i])
             
     return len(answer)
