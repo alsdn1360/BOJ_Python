b5e0c6bd55fa8d77aab7982878a4725a98988199
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-25 20:51:17 +0900

    [level 2] Title: 주식가격, Time: 27.10 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,21 +1,18 @@
 def solution(prices):
     n = len(prices)
-    stack = [] # prices의 인덱스가 들어감
-    seconds = [0] * n
     
-    # i는 현재 시간
-    for i in range(n):
-        # 스택이 비어있지 않고
-        # 현재 시간의 가격이 스택의 최상위 인덱스의 가격보다 작으면 가격이 떨어진 것
-        while stack and prices[stack[-1]] > prices[i]:
-            index = stack.pop()
-            seconds[index] = i - index # 현재 시간 - 이전의 인덱스(최상위 인덱스)
+    answer = [0] * n
+    stack = []
+    
+    for time, price in enumerate(prices):
+        while stack and price < prices[stack[-1]]:
+            past_time = stack.pop()
+            answer[past_time] = time - past_time
             
-        stack.append(i)
-    
-    # 스택에 남아있는 인덱스 처리
+        stack.append(time)
+        
     while stack:
-        index = stack.pop()
-        seconds[index] = n - index - 1 # 현재 위치를 제외하고 남은 시간을 위해 -1을 함
-                
-    return seconds
+        past_time = stack.pop()
+        answer[past_time] = n - 1 - past_time
+        
+    return answer
