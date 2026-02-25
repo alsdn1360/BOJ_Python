b312a3d8c4c716435710519b633b00dafa284c7c
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-25 18:24:55 +0900

    [level 2] Title: 프로세스, Time: 0.27 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
 
 ### 성능 요약
 
-메모리: 62.3 MB, 시간: 9.18 ms
+메모리: 9.27 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 23일 10:49:02
+2026년 02월 25일 18:24:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,27 +2,27 @@ from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    queue = deque()
     
-    # 큐에 우선순위와 그때의 위치를 삽입
-    for idx, priority in enumerate(priorities):
-        queue.append((idx, priority))
+    n = len(priorities)
+    
+    target_process = deque([False] * n)
+    target_process[location] = True
+    
+    priorities = deque(priorities)
+    
+    run_process = max(priorities)
+    
+    while priorities:
+        process, is_target = priorities.popleft(), target_process.popleft()
         
-    while queue:
-        # 위치와 우선순위 팝
-        l, p = queue.popleft()
-        
-        # 현재 우선순위가 가장 높은 것을 확인함
-        high_p = max(priorities)
-        
-        # 현재 우선순위가 더 높은게 있다면 다시 삽입
-        if high_p > p:
-            queue.append((l, p))
-        else:
-            # 현재 우선순위랑 같거나 더 크면
-            priorities.remove(high_p)
+        if process >= run_process:
             answer += 1
             
-            # 그때 원하는 위치면 리턴
-            if location == l:
+            if is_target:
                 return answer
+            
+            run_process = max(priorities)
+        else:      
+            priorities.append(process)
+            target_process.append(is_target)
+    
