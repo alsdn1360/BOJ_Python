2ddb7e91032404042a24b727aa4739abf097e518
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-25 18:57:37 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 137.44 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 410.67 ms
+메모리: 9.3 MB, 시간: 137.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 22일 22:51:56
+2026년 02월 25일 18:57:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,39 +1,30 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    num_of_truck = len(truck_weights) # 트럭들이 전부 다리를 건넜는지 체크하기 위함
+    time = 0
+    passing = deque()
+    waiting = deque(truck_weights)
+    passed = 0
     
-    waiting_truck = deque(truck_weights)
-    crossing_truck = deque()
-    crossing_truck_second = deque()
-    crossed_truck = deque()
+    curr_weight = 0
+    nxt_weight = waiting[0]
     
-    seconds = 0
-    current_bridge_weight = 0
-    
-    # 다리를 지난 트럭의 수와 대기했던 트럭의 수가 같으면 종료
-    while len(crossed_truck) != num_of_truck:
-        seconds += 1
+    while passed < len(truck_weights):
+        time += 1
         
-        if crossing_truck:
-            # 다리를 건너는 첫 번째 트럭이 다리 길이와 같으면 다 건넌 것이므로 다리를 지난 것으로 판단
-            if crossing_truck_second[0] == bridge_length:
-                crossing_truck_second.popleft()
-                
-                current_crossed_truck = crossing_truck.popleft()
-                current_bridge_weight -= current_crossed_truck
-                crossed_truck.append(current_crossed_truck)
+        if passing and time - passing[0][0] == bridge_length:
+            _, passed_weight = passing.popleft()
+            passed += 1
             
-            # 다리를 건너는 트럭이 1초마다 이동하기 때문에 1씩 증가
-            for i in range(len(crossing_truck_second)):
-                crossing_truck_second[i] += 1
-            
-        # 다리를 건너는 트럭의 무게와 대기 중인 첫 번째 트럭의 무게를 더했을 때, 한도 무게보다 작으면 추가
-        if waiting_truck and current_bridge_weight + waiting_truck[0] <= weight:
-            crossing_truck_second.append(1) 
-            
-            current_truck = waiting_truck.popleft()
-            current_bridge_weight += int(current_truck)
-            crossing_truck.append(int(current_truck))
+            curr_weight -= passed_weight
         
-    return seconds
+        if waiting and curr_weight + nxt_weight <= weight:
+            curr_weight += nxt_weight
+            passing.append((time, nxt_weight))
+            
+            waiting.popleft()
+            
+            if waiting:
+                nxt_weight = waiting[0]
+            
+    return time
