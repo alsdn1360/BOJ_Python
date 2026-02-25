06b0f16ff017232aed12479f5b5be83155830803
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-25 17:23:50 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,37 +1,25 @@
-from collections import deque
+from math import ceil
 
 def solution(progresses, speeds):
-    queue = deque()
     answer = []
-    pre_feature = 0
     
-    for i in range(len(progresses)):
-        remain_progress = 100 - progresses[i]
+    n = len(progresses)
+    
+    deploy_days = []
+    
+    for i in range(n):
+        p, s = progresses[i], speeds[i]
+        deploy_days.append(ceil((100 - p) / s))
         
-        # 배포 가능 날짜를 구함
-        deploy_day = remain_progress // speeds[i]
+    l, r = 0, 1
     
-        # 날짜 올림 해줌
-        if remain_progress % speeds[i] > 0:
-            deploy_day += 1
-            
-        queue.append(deploy_day)
-    
-    while queue:
-        feature = queue.popleft()
-        
-        # 이전에 배포한 날짜가 현재 배포한 날짜보다 크면 이후의 기능도 배포 가능하기 때문에 추가
-        if feature <= pre_feature:
-            deploy_feature += 1
+    while l <= r and r < n:
+        if deploy_days[l] < deploy_days[r]:
+            answer.append(r - l)
+            l = r
         else:
-            # 현재 배포한 날짜가 더 크기 때문에 아직 배포할 수 없어서 지금까지 배포한 기능의 수 추가
-            if pre_feature != 0:
-                answer.append(deploy_feature)
+            r += 1
             
-            pre_feature = feature
-            deploy_feature = 1
-    
-    # 마지막으로 기능 수 추가
-    answer.append(deploy_feature)
+    answer.append(r - l)
         
     return answer
