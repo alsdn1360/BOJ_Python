c073d3ffe209070e5f08349da30a86378d0de147
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-25 22:28:12 +0900

    [level 2] Title: 더 맵게, Time: 1875.22 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,23 +1,22 @@
 import heapq
 
+
 def solution(scoville, K):
     answer = 0
-    n = len(scoville)
-    
-    # scoville을 우선순위 큐로 변경
     heapq.heapify(scoville)
     
-    # 가장 작은 스코빌 지수가 K 보다 크거나 같을 때까지 반복
-    while scoville[0] < K:
-        # 계속 반복해도 모든 음식의 개수가 1이되면 K 이상으로 만들 수 없으므로 -1 리턴
-        if len(scoville) == 1:
+    while scoville:
+        first_s = heapq.heappop(scoville)
+        
+        if first_s >= K:
+            return answer
+        
+        if not scoville:
             return -1
-        
-        scoville1 = heapq.heappop(scoville)
-        scoville2 = heapq.heappop(scoville)
-        
-        heapq.heappush(scoville, scoville1 + (scoville2 * 2))
-        
+            
+        second_s = heapq.heappop(scoville)
+                           
+        heapq.heappush(scoville, first_s + (second_s * 2))
         answer += 1
-        
-    return answer
+                           
+    return -1
