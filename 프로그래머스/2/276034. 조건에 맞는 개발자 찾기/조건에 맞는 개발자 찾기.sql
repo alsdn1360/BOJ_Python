f174faea321d32e0e64ff5883b970d0ad517edac
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-26 15:52:32 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,11 @@
+SELECT
+    id,
+    email,
+    first_name,
+    last_name
+FROM
+    developers
+WHERE
+    skill_code & (SELECT SUM(code) FROM skillcodes WHERE name IN ('Python', 'C#'))
+ORDER BY
+    id ASC
