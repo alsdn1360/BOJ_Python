2ca3fb2bf25f20d38471e04c2d37e23b2ff6e27d
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-26 22:33:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 7257.34 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
 
 ### 성능 요약
 
-메모리: 15.4 MB, 시간: 286.08 ms
+메모리: 15.5 MB, 시간: 7257.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 22:15:23
+2026년 02월 26일 22:33:25
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,22 +1,44 @@
 import heapq
 
+
+def pop_max(heap):
+    temp_heap = []
+                    
+    while heap:
+        heapq.heappush(temp_heap, -heapq.heappop(heap))
+
+    max_num = -heapq.heappop(temp_heap)
+
+    while temp_heap:
+        heapq.heappush(heap, -heapq.heappop(temp_heap))
+        
+    return max_num, heap
+
+
 def solution(operations):
     heap = []
     
     for operation in operations:
-        cmd, num = operation.split()
+        command, num = operation.split(' ')
         
-        if cmd == 'I':
-            # 우선순위큐로 삽입
+        if command == 'I':
             heapq.heappush(heap, int(num))
-        elif cmd == 'D' and len(heap) != 0:
-            if num == '-1':
-                # 우선순위큐이므로 가장 작은 값 팝
-                heapq.heappop(heap)
-            else:
-                heap.remove(max(heap))
-                    
+        elif command == 'D':
+            if heap:
+                if num == '-1':
+                    heapq.heappop(heap)
+                elif num == '1':
+                    _, heap = pop_max(heap)
+    
     if not heap:
         return [0, 0]
+    elif len(heap) == 1:
+        num = heapq.heappop(heap)
+        
+        return [num, num]
+    else:
+        max_num, heap = pop_max(heap)
+        min_num = heapq.heappop(heap)
+        
+        return [max_num, min_num]
     
-    return [max(heap), heap[0]]
