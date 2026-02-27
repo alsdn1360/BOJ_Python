9752a3b67843503b5b5945ecdf56aad5d5b2e1a0
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-27 12:31:41 +0900

    [level 1] Title: 최소직사각형, Time: 2.06 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 18.99 ms
+메모리: 10.8 MB, 시간: 2.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 21일 19:51:29
+2026년 02월 27일 12:31:38
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,18 +1,13 @@
 def solution(sizes):
-    answer = 0
-    
-    # 가로, 세로 값을 각각 하나의 리스트에 저장하기 위함
-    r_sizes = [[] for _ in range(2)]
-    
-    n = len(sizes)
-    
-    # 작은 값은 가로에 넣고 큰 값은 세로에 넣음
-    for i in range(n):
-        if sizes[i][0] > sizes[i][1]:
-            r_sizes[0].append(sizes[i][1])
-            r_sizes[1].append(sizes[i][0])
+    w = []
+    h = []
+
+    for size in sizes:
+        if size[0] > size[1]:
+            w.append(size[0])
+            h.append(size[1])
         else:
-            r_sizes[0].append(sizes[i][0])
-            r_sizes[1].append(sizes[i][1])
+            w.append(size[1])
+            h.append(size[0])
     
-    return max(r_sizes[0]) * max(r_sizes[1])
+    return max(w) * max(h)
