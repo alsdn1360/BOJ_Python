cfddcd93d9b06dcec45f1b109e368e2d7e8077a2
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-27 16:08:59 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.24 MB, 시간: 0.01 ms
+메모리: 9.25 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 28일 11:13:13
+2026년 02월 27일 16:08:57
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,10 @@
 def solution(word):
     answer = 0
     
-    alpha = ['A', 'E', 'I', 'O', 'U']
-    
-    # 그 자리 뒤에 올 수 있는 모든 가능한 단어들의 개수
-    # [(5⁰ + 5¹ + 5² + 5³ + 5⁴), (5⁰ + 5¹ + 5² + 5³), ...]
+    vowel = ['A', 'E', 'I', 'O', 'U']
     weight = [781, 156, 31, 6, 1]
     
-    for i, w in enumerate(word):
-		    # 각 알파벳마다 781번 건너뛰게 되므로 알파벳의 인덱스를 곱해줌
-        answer += alpha.index(w) * weight[i]
-    
-    return answer + len(word)
+    for i, c in enumerate(word):
+        answer += weight[i] * vowel.index(c) + 1
+
+    return answer
