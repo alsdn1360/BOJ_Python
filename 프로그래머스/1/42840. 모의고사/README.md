37d83d2399fe6218638b39208eb881e430d3cb59
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-27 12:58:40 +0900

    [level 1] Title: 모의고사, Time: 2.49 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 66.8 MB, 시간: 17.83 ms
+메모리: 9.43 MB, 시간: 2.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 23:47:59
+2026년 02월 27일 12:58:39
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,24 @@
 def solution(answers):
     answer = []
-    patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
-    scores = [0] * 3
     
-    for i, answer in enumerate(answers):
-        for j, pattern in enumerate(patterns):
-            if answer == pattern[i % len(pattern)]:
-                scores[j] += 1
-                
-    max_score = max(scores)
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    n1, n2, n3 = len(p1), len(p2), len(p3)
+    cnt = [0] * 3
     
-    highest_scores = []
-    
-    for i, score in enumerate(scores):
-        if max_score == score:
-            highest_scores.append(i + 1)
+    for i, ans in enumerate(answers):
+        if ans == p1[i % n1]:
+            cnt[0] += 1
+        if ans == p2[i % n2]:
+            cnt[1] += 1
+        if ans == p3[i % n3]:
+            cnt[2] += 1
             
-    return highest_scores
+    max_cnt = max(cnt)
+    
+    for i, c in enumerate(cnt):
+        if max_cnt == c:
+            answer.append(i + 1)
+    
+    return answer
