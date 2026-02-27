73f8138263e4d9e2e4c4efd2ea86513dd943ea01
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-27 16:36:15 +0900

    [level 1] Title: 폰켓몬, Time: 1.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,16 +1,4 @@
 from collections import defaultdict
 
-def solution(nums):
-    answer = 0
-    
-    choice_cnt = len(nums) // 2
-    
-    ponkemon = defaultdict(int)
-    
-    for num in nums:
-        ponkemon[num] = 1
-
-    for cnt in ponkemon.values():
-        answer += 1
-    
-    return answer if answer <= choice_cnt else choice_cnt
+def solution(nums):    
+    return min(len(nums) / 2, len(set(nums)))
