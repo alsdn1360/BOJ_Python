00ad4b7a727a7c438e313846bef956b092845cd4
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-02-27 16:22:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.10 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,20 @@
+from collections import defaultdict
+
+
 def solution(participant, completion):
-    dic = {}
+    participant_status = defaultdict(int)
     
     for p in participant:
-        if p in dic:
-            dic[p] += 1
-        else:
-            dic[p] = 1
+        if participant_status[p]:
+            participant_status[p] += 1
+            continue
             
-    for c in completion:
-        dic[c] -= 1
+        participant_status[p] = 1
         
-    for key in dic:
-        if dic[key] > 0:
-            return key
+    for c in completion:
+        participant_status[c] -= 1
+        
+    for p, c in participant_status.items():
+        if c > 0:
+            return p
+    
