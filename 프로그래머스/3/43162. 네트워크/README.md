628c6a07ea3ba9b7aef349765f89157569da6d71
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-03-02 17:06:47 +0900

    [level 3] Title: 네트워크, Time: 0.84 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 65.7 MB, 시간: 4.51 ms
+메모리: 9.33 MB, 시간: 0.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 21:56:21
+2026년 03월 02일 17:06:44
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,33 +1,36 @@
-from collections import defaultdict
-
-def dfs(adj_list, curr_computer, visited):
-    # 현재 컴퓨터 방문 처리
-    visited.add(curr_computer)
+from collections import deque
     
-    # 현재 컴퓨터에 인접한 컴퓨터 불러오기
-    for adj_computer in adj_list[curr_computer]:
-        # 그 중에서 방문하지 않은 컴퓨터면 dfs 실행
-        if adj_computer not in visited:
-            dfs(adj_list, adj_computer, visited)
 
 def solution(n, computers):
     answer = 0
-
-    # 각 컴퓨터와 연결된 인접한 컴퓨터 리스트
-    adj_list = defaultdict(list)
-    # 방문한 컴퓨터 초기화
-    visited = set()
     
-    # computers 중에서 1이면 연결이 되어있으므로 현재 컴퓨터와 연결
+    networks = {c:set() for c in range(n)}
+    
     for i in range(n):
         for j in range(n):
-            if i != j and computers[i][j] == 1:
-                adj_list[i].append(j)
-                
-    # 아직 방문하지 않은 컴퓨터는 연결되어 있지 않은 것이므로 네트워크 +1, 이후 그 컴퓨터에 대한 dfs 실행
-    for i in range(n):
-        if i not in visited:
+            if i == j:
+                continue
+            
+            if computers[i][j] == 1:
+                networks[i].add(j)
+                networks[j].add(i)
+            
+    visited = [False] * n
+            
+    for c in networks.keys():
+        if not visited[c]:
+            # bfs
             answer += 1
-            dfs(adj_list, i, visited)
+            
+            queue = deque([c])
+            visited[c] = True
+            
+            while queue:
+                nxt_c = queue.popleft()
                 
+                for adj_c in networks[nxt_c]:
+                    if not visited[adj_c]:
+                        queue.append(adj_c)
+                        visited[adj_c] = True
+    
     return answer
