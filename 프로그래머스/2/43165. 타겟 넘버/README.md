73a56162bfdf83526dfe50f96e4e960cd0f72c11
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-03-02 15:58:45 +0900

    [level 2] Title: 타겟 넘버, Time: 308.66 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 62.4 MB, 시간: 20.22 ms
+메모리: 9.23 MB, 시간: 308.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 21:30:48
+2026년 03월 02일 15:58:42
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,21 @@
-def dfs(idx, numbers, target, total):
-    if idx == len(numbers):
-        return 1 if total == target else 0
+def bt(n, numbers, idx, current, target):    
+    global answer
+    
+    if idx == n:
+        if current == target:
+            answer += 1
+        
+        return
+        
+    bt(n, numbers, idx + 1, current + numbers[idx], target)
+    bt(n, numbers, idx + 1, current - numbers[idx], target)
+    
 
-    # 양수로 더하는 경우랑 음수로 더하는 경우를 dfs에 둘 다 넣어서 계산함
-    return dfs(idx + 1, numbers, target, total + numbers[idx]) + dfs(idx + 1, numbers, target, total - numbers[idx])
-            
 def solution(numbers, target):
-    return dfs(0, numbers, target, 0)
+    global answer
+    
+    answer = 0
+    
+    bt(len(numbers), numbers, 0, 0, target)
+    
+    return answer
