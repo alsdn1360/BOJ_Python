a1a6e503a19bc8d64e2d080f2943f043449b3cf7
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-03-05 15:20:33 +0900

    [level 3] Title: 가장 먼 노드, Time: 53.02 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,35 +1,25 @@
 from collections import defaultdict, deque
 
 
-def solution(n, edge):
-    answer = 0
-    
+def solution(n, edge):  
     graph = defaultdict(list)
     
     for u, v in edge:
         graph[u].append(v)
         graph[v].append(u)
     
-    dists = [float('inf')] * (n + 1)
-        
     # BFS
-    queue = deque([(1, 1)])
+    queue = deque([1])
     
-    visited = set()
-    visited.add(1)
+    dists = [-1] * (n + 1)
+    dists[1] = 0
     
     while queue:
-        node, cnt = queue.popleft()
-        
-        dists[node] = min(dists[node], cnt)
+        node = queue.popleft()
             
         for adj_node in graph[node]:
-            if adj_node not in visited:
-                queue.append((adj_node, cnt + 1))
-                visited.add(adj_node)
-                
-    max_dist = max(dists[1:])
+            if dists[adj_node] == -1:
+                queue.append(adj_node)
+                dists[adj_node] = dists[node] + 1
     
-    answer = dists.count(max_dist)
-    
-    return answer
+    return dists.count(max(dists))
